04519c74afeb8de3fde4fa713070a9c750d10b80
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-09-22 14:44:28 -0600

    Add category list

--- src/GifExploreApp.jsx
+++ src/GifExploreApp.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 
 export const GifExploreApp = () => {
+
+  const [categories, setCategories] = useState(['Pucca', 'Garu','Bang bang']);
+
+
   return (
     <>
         <h1>GifExploreApp</h1>
+        <ol>
+          {categories.map( category => {
+              return <li key={ category }>{ category }</li>
+            })
+          }
+        </ol>
     </>
   )
 }
