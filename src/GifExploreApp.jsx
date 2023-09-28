0a7a5c673dc1c306a0a13fc4024411d709266f54
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-09-28 15:38:50 -0600

    Add new category

--- src/GifExploreApp.jsx
+++ src/GifExploreApp.jsx
@@ -4,10 +4,16 @@ export const GifExploreApp = () => {
 
   const [categories, setCategories] = useState(['Pucca', 'Garu','Bang bang']);
 
+  const onAddCategory = () => {
+    //Add new category - valorant 
+    setCategories([...categories, 'Variant']);
+    //  setCategories( cat => [...cat, 'valorant']);
+  }
 
   return (
     <>
         <h1>GifExploreApp</h1>
+        <button onClick={onAddCategory}>Agregar</button>
         <ol>
           {categories.map( category => {
               return <li key={ category }>{ category }</li>
